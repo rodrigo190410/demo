e59f33bd7bff5376e41ce7130ece74b93928fdf7
Bruno-Guerrero <brunomanager2006@gmail.com>
2026-05-07 00:23:01 -0500

    Feat: Querys Native SQL

--- src/main/java/com/example/demo/controller/CustomerController.java
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -1,4 +1,41 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.CustomerQueryDTO;
+import com.example.demo.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/seedair/customers/") // http://localhost:8080/seedair
 public class CustomerController {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    // 1.GET: http://localhost:8080/customers/query1
+    @GetMapping("/query1")
+    public List<CustomerQueryDTO> getCustomersNoReservation() {
+        return customerRepository.findCustomersWithParcelButNoReservation();
+    }
+
+    // 2.GET: http://localhost:8080/customers/query2/0
+    @GetMapping("/query2/{cantidad}")
+    public List<CustomerQueryDTO> getCustomersMoreThanParcels(@PathVariable Integer cantidad) {
+        return customerRepository.findCustomersWithMoreThanXParcels(cantidad);
+    }
+
+    // 3.GET: http://localhost:8080/customers/query3/
+    @GetMapping("/query3")
+    public List<CustomerQueryDTO> getCustomersNoReviews() {
+        return customerRepository.findCustomersWithParcelAndNoReviews();
+    }
+
+    // 4.GET: http://localhost:8080/customers/query4/4.5
+    @GetMapping("/query4/{rating}")
+    public List<CustomerQueryDTO> getCustomersHighRating(@PathVariable Double rating) {
+        return customerRepository.findCustomersWithVisibleReviewAndRatingGreaterThan(rating);
+    }
 }
--- src/main/java/com/example/demo/repositories/CustomerRepository.java
+++ src/main/java/com/example/demo/repositories/CustomerRepository.java
@@ -1,7 +1,42 @@
 package com.example.demo.repositories;
 
+import com.example.demo.dto.CustomerQueryDTO;
 import com.example.demo.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    @Query(value = "SELECT DISTINCT c.first_name as firstName, c.last_name as lastName, c.phone as phone " +
+            "FROM customers c " +
+            "INNER JOIN parcels p ON c.id = p.customer_id " +
+            "LEFT JOIN reservations r ON p.id = r.parcel_id " +
+            "WHERE r.id IS NULL", nativeQuery = true)
+    List<CustomerQueryDTO> findCustomersWithParcelButNoReservation();
+
+
+    @Query(value = "SELECT c.first_name as firstName, c.last_name as lastName, c.phone as phone " +
+            "FROM customers c " +
+            "INNER JOIN parcels p ON c.id = p.customer_id " +
+            "GROUP BY c.id, c.first_name, c.last_name, c.phone " +
+            "HAVING COUNT(p.id) > :cantidad", nativeQuery = true)
+    List<CustomerQueryDTO> findCustomersWithMoreThanXParcels(@Param("cantidad") Integer cantidad);
+
+
+    @Query(value = "SELECT DISTINCT c.first_name as firstName, c.last_name as lastName, c.phone as phone " +
+            "FROM customers c " +
+            "INNER JOIN parcels p ON c.id = p.customer_id " +
+            "WHERE c.id NOT IN (SELECT customer_id FROM reviews)", nativeQuery = true)
+    List<CustomerQueryDTO> findCustomersWithParcelAndNoReviews();
+
+
+    @Query(value = "SELECT DISTINCT c.first_name as firstName, c.last_name as lastName, c.phone as phone " +
+            "FROM customers c " +
+            "INNER JOIN reviews r ON c.id = r.customer_id " +
+            "WHERE r.is_visible = true AND r.rating > :ratingValue", nativeQuery = true)
+    List<CustomerQueryDTO> findCustomersWithVisibleReviewAndRatingGreaterThan(@Param("ratingValue") Double ratingValue);
+
 }
